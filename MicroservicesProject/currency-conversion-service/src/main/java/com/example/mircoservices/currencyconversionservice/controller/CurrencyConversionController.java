4fd555303e9f6a60fd78ea2ccab036ccef75c45f
Rupesh-01 <rupeshsrivastav19@gmail.com>
2024-04-27 11:31:04 +0530

    adding distributed logging tracing using zipkins

--- MicroservicesProject/currency-conversion-service/src/main/java/com/example/mircoservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ MicroservicesProject/currency-conversion-service/src/main/java/com/example/mircoservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -10,22 +10,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.mircoservices.currencyconversionservice.builders.RestTemplateConfiguration;
 import com.example.mircoservices.currencyconversionservice.entities.CurrencyConversion;
 import com.example.mircoservices.currencyconversionservice.proxies.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
     private CurrencyExchangeProxy currencyExchangeProxy;
+    private RestTemplate restTemplate;
     @Autowired
-    public CurrencyConversionController(CurrencyExchangeProxy theCurrencyExchangeProxy){
+    public CurrencyConversionController(CurrencyExchangeProxy theCurrencyExchangeProxy, RestTemplate theRestTemplate){
         this.currencyExchangeProxy = theCurrencyExchangeProxy;
+        this.restTemplate = theRestTemplate;
     }
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculatedCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
         HashMap<String,String> uriVariables = new HashMap<>();
         uriVariables.put("from",from);
         uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity("http://localhost:8080/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
         CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(currencyConversion.getId(),from,to,quantity, currencyConversion.getConversionValue(), quantity.multiply(currencyConversion.getConversionValue()),currencyConversion.getEnvironment()+"rest");
     }
--- MicroservicesProject/currency-exchange-service/src/main/java/com/example/mircroservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ MicroservicesProject/currency-exchange-service/src/main/java/com/example/mircroservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.example.mircroservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import com.example.mircroservices.currencyexchangeservice.entities.CurrencyExcha
 
 @RestController
 public class CurrencyExchangeController {
+    private Logger logger= LoggerFactory.getLogger(CircuitBreakerController.class);
     private Environment environment;
     @Autowired
     public CurrencyExchangeController(Environment environment) {
@@ -20,6 +23,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+        logger.info("retrieveExchangeValue called with {} to {}",from, to);
         CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50));
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
         return currencyExchange;
